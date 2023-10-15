905f57f11e54e9061019f73879c3452bbef688e6
Graham Reeves <graham@grahamreeves.com>
2023-10-15 16:02:23 +0100

    Fixes for ios framework

--- Source_Apple/BuildUniversal.sh
+++ Source_Apple/BuildUniversal.sh
@@ -18,6 +18,8 @@ BUILDPATH_SIM="${BUILD_DIR}/${PROJECT_NAME}_IosSimulator"
 BUILDPATH_MACOS="${BUILD_DIR}/${PROJECT_NAME}_Macos"
 BUILDPATH_TVOS="${BUILD_DIR}/${PROJECT_NAME}_Tvos"
 
+SCHEME_IOS=${PROJECT_NAME}_IosFramework
+
 PRODUCT_NAME_UNIVERSAL="${PROJECT_NAME}.xcframework"
 
 CONFIGURATION="Release"
@@ -26,7 +28,7 @@ DESTINATION_IOS="generic/platform=iOS"
 # build archived frameworks
 # see https://github.com/NewChromantics/PopAction_BuildApple/blob/master/index.js for some battle tested CLI options
 echo "Building sub-framework archives..."
-xcodebuild archive -scheme ${PROJECT_NAME}_Ios -archivePath $BUILDPATH_IOS SKIP_INSTALL=NO -sdk iphoneos -configuration ${CONFIGURATION} -destination ${DESTINATION_IOS}
+xcodebuild archive -scheme ${SCHEME_IOS} -archivePath $BUILDPATH_IOS SKIP_INSTALL=NO -sdk iphoneos -configuration ${CONFIGURATION} -destination ${DESTINATION_IOS}
 #xcodebuild archive -scheme ${PROJECT_NAME}_IosSimulator -archivePath $BUILDPATH_SIM SKIP_INSTALL=NO -sdk iphonesimulator
 #xcodebuild archive -scheme ${PROJECT_NAME}_Macos -archivePath $BUILDPATH_MACOS SKIP_INSTALL=NO
 #xcodebuild archive -scheme ${PROJECT_NAME}_Tvos -archivePath $BUILDPATH_TVOS SKIP_INSTALL=NO
